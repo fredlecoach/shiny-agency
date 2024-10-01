60c072aa454540a7353695cf2e299985124dbcad
roblot <f.roblot.coulanges@gmail.com>
2024-10-01 13:19:12 +0200

    router et link

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,12 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import '../styles/App.css';
 import Home from './Home';
 import Survey from './Survey';
+import Header from'./Header';
 
 function App() {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/survey" element={<Survey />} />
